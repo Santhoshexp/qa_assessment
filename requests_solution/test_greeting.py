ef840e29805b742c04f15ed9b1c12d9dd9ab49b3
Santhoshexp <53528710+Santhoshexp@users.noreply.github.com>
2024-09-13 12:53:10 +0530

    Update test_greeting.py

--- requests_solution/test_greeting.py
+++ requests_solution/test_greeting.py
@@ -1,16 +1,16 @@
-"""Module"""
+"""Module to test the frontend service"""
 
 import requests
 
 def test_response():
-    """Method"""
+    """Method to validate the response code"""
     url = "http://127.0.0.1:56563"
     response = requests.get(url,timeout=60)
     assert response.status_code == 200
     assert "<h1>Hello from the Backend!</h1>" in response.text
 
 def test_content():
-    """Method"""
+    """Method to validate the response content"""
     url = "http://127.0.0.1:56563"
     response = requests.get(url,timeout=60)
     assert "<h1>Hello from the Backend!</h1>" == response.text
